26d77c06d7a888c006336ab7ecc1310236cf5e1f
DonStylz74 <84778838+DonStylz74@users.noreply.github.com>
2024-04-21 21:53:41 +1000

    Update extra.sql

--- extra.sql
+++ extra.sql
@@ -62,8 +62,8 @@ CREATE TABLE  `owned_vehicles` (
 ALTER TABLE `owned_vehicles`
     ADD COLUMN `infoimpound` longtext DEFAULT NULL,
     ADD COLUMN `lastparking` longtext DEFAULT NULL,
-    ADD COLUMN `friends` longtext DEFAULT NULL;
-    ADD COLUMN `fakeplate` varchar(12) DEFAULT NULL,
+    ADD COLUMN `friends` longtext DEFAULT NULL,
+    ADD COLUMN `fakeplate` varchar(12) DEFAULT NULL;
 
 -- sna fuel
 ALTER TABLE `owned_vehicles`
