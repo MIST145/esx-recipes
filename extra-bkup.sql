b9867cc5320448bfbbcd9a874082ea4dde8aac12
DonStylz74 <84778838+DonStylz74@users.noreply.github.com>
2024-04-23 09:47:14 +1000

    Update extra.sql

--- extra-bkup.sql
+++ extra-bkup.sql
@@ -29,24 +29,6 @@ CREATE TABLE IF NOT EXISTS `player_transactions` (
   PRIMARY KEY (`id`)
 );
 
--- mono garage
-CREATE TABLE  `owned_vehicles` (
-  `owner` varchar(60) DEFAULT NULL,
-  `plate` varchar(12) NOT NULL,
-  `vehicle` longtext DEFAULT NULL,
-  `type` varchar(20) NOT NULL DEFAULT 'car',
-  `job` varchar(20) DEFAULT NULL,
-  `stored` tinyint(4) NOT NULL DEFAULT 0,
-  `parking` VARCHAR(60) DEFAULT NULL,
-  `pound` VARCHAR(60) DEFAULT NULL
-) ENGINE=InnoDB;
-
-ALTER TABLE `owned_vehicles`
-    ADD COLUMN `infoimpound` longtext DEFAULT NULL,
-    ADD COLUMN `lastparking` longtext DEFAULT NULL,
-    ADD COLUMN `friends` longtext DEFAULT NULL,
-    ADD COLUMN `fakeplate` varchar(12) DEFAULT NULL;
-
 -- sna fuel
 ALTER TABLE `owned_vehicles`
    ADD COLUMN `datetimein` INT DEFAULT 0;
