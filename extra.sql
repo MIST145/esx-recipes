342b8b1c568e220069e6b2042f58cc29aceb90c8
DonStylz74 <84778838+DonStylz74@users.noreply.github.com>
2024-04-24 11:52:31 +1000

    Update extra.sql

--- extra.sql
+++ extra.sql
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS `player_transactions` (
 ALTER TABLE `owned_vehicles`
     ADD COLUMN `infoimpound` longtext DEFAULT NULL,
     ADD COLUMN `lastparking` longtext DEFAULT NULL,
-    ADD COLUMN `friends` longtext DEFAULT NULL;
+    ADD COLUMN `friends` longtext DEFAULT NULL,
     ADD COLUMN `fakeplate` varchar(12) DEFAULT NULL;
 
 -- sna fuel
