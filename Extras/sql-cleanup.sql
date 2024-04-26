44d1a1b77f19a1373b8db7d78b1e9fbb240db590
DonStylz74 <84778838+DonStylz74@users.noreply.github.com>
2024-04-27 02:20:39 +1000

    Update sql-cleanup.sql

--- Extras/sql-cleanup.sql
+++ Extras/sql-cleanup.sql
@@ -1,1 +1,8 @@
+-- Dropping vehicles table to add Extra Vehicles
 DROP TABLE `vehicles`;
+
+-- Dropping Data from Datastore table --  Removing esx_accessories data entrys
+DELETE FROM datastore WHERE name = 'user_ears';
+DELETE FROM datastore WHERE name = 'user_glasses';
+DELETE FROM datastore WHERE name = 'user_helmet';
+DELETE FROM datastore WHERE name = 'user_mask';
