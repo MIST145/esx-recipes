8308fc27048ebb3c1433bf45dfbbaa69cf4003bc
DonStylz74 <84778838+DonStylz74@users.noreply.github.com>
2024-04-27 02:42:17 +1000

    Update sql-cleanup.sql

--- Extras/sql-cleanup.sql
+++ Extras/sql-cleanup.sql
@@ -6,3 +6,8 @@ DELETE FROM datastore WHERE name = 'user_ears';
 DELETE FROM datastore WHERE name = 'user_glasses';
 DELETE FROM datastore WHERE name = 'user_helmet';
 DELETE FROM datastore WHERE name = 'user_mask';
+
+-- Dropping esx_banker Data from Datastore tables --  Removing esx_banker data entrys
+DELETE FROM addon_account WHERE name = 'society_banker';
+DELETE FROM jobs WHERE name = 'banker';
+DELETE FROM job_grades WHERE job_name = 'banker';
