7a1c589ce3db4ff18e7c9445b762dc27becc13ea
DonStylz74 <84778838+DonStylz74@users.noreply.github.com>
2024-05-18 22:49:48 +1000

    Update sql-inject.sql

--- Extras/sql-inject.sql
+++ Extras/sql-inject.sql
@@ -1,7 +1,7 @@
 INSERT INTO `vehicle_categories` (name, label) VALUES
 	('customs','Customs');
 
--- Mono mVehicle & mGarage Injection
+-- Mono mVehicle & mGarage SQL Injection
 -- esx-sna-fuel Injection
 CREATE TABLE IF NOT EXISTS `owned_vehicles` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -24,6 +24,25 @@ CREATE TABLE IF NOT EXISTS `owned_vehicles` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
--- renzu scoreboard sql
+-- renzu scoreboard SQL Injection
 ALTER TABLE `users`
 ADD avatar mediumtext NULL;
+
+-- IT_Drugs SQL Injection
+-- Install the drug_plants table
+CREATE TABLE IF NOT EXISTS drug_plants (id INT(11) NOT NULL AUTO_INCREMENT, PRIMARY KEY(id));
+ALTER TABLE drug_plants ADD IF NOT EXISTS coords LONGTEXT NOT NULL;
+ALTER TABLE drug_plants ADD IF NOT EXISTS time INT(255) NOT NULL;
+ALTER TABLE drug_plants ADD IF NOT EXISTS type VARCHAR(100) NOT NULL;
+ALTER TABLE drug_plants ADD IF NOT EXISTS health DOUBLE NOT NULL DEFAULT 100;
+ALTER TABLE drug_plants ADD IF NOT EXISTS fertilizer DOUBLE NOT NULL DEFAULT 0;
+ALTER TABLE drug_plants ADD IF NOT EXISTS water DOUBLE NOT NULL DEFAULT 0;
+ALTER TABLE drug_plants ADD IF NOT EXISTS growtime INT(11) NOT NULL;
+
+-- Install the drug_processing table
+CREATE TABLE IF NOT EXISTS drug_processing (id INT(11) NOT NULL AUTO_INCREMENT, PRIMARY KEY(id));
+ALTER TABLE drug_processing ADD IF NOT EXISTS coords LONGTEXT NOT NULL;
+ALTER TABLE drug_processing ADD IF NOT EXISTS rotation DOUBLE NOT NULL;
+ALTER TABLE drug_processing ADD IF NOT EXISTS type VARCHAR(100) NOT NULL;
+
+
