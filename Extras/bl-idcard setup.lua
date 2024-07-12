cb1dd87f3db8b021a4152bf4c650351d6b150af3
DonStylz74 <84778838+DonStylz74@users.noreply.github.com>
2024-07-12 20:51:28 +1000

    Add files via upload

---
+++ Extras/bl-idcard setup.lua
@@ -0,0 +1,95 @@
+    items = {
+        ['id_card'] = {
+            prop = `prop_franklin_dl`,
+            genderIdType = {
+                female = 'female_id',
+                male = 'male_id'
+            }
+        },
+        
+        ['license_drive'] = {
+            prop = `prop_franklin_dl`,
+            idType = 'license_drive'
+        },
+
+        ['license_weapon'] = {
+            prop = `prop_franklin_dl`,
+            idType = 'license_weapon'
+        },
+    },
+
+
+
+    idTypes = {
+        driver_license = {
+            type = 'license_drive',
+            title = 'SAN ANDREAS',
+            titleColour = '#bdbdbd',
+
+            label = 'DRIVER LICENSE',
+        
+            stamp = true,
+            profileStamp = false,
+        
+            signature = true,
+        
+            bgColour = '#000',
+            bgColourSecondary = '#000',
+        
+            textColour = '#FFF',
+        },
+
+        weapon_license = {
+            type = 'license_weapon',
+            title = 'SAN ANDREAS',
+            titleColour = '#ff4538',
+
+            label = 'WEAPON LICENSE',
+        
+            stamp = true,
+            profileStamp = false,
+        
+            signature = true,
+        
+            bgColour = '#460000',
+            bgColourSecondary = '#E90000',
+        
+            textColour = '#FFF',
+        },
+
+        female_id = {
+            type = 'female_id',
+            title = 'SAN ANDREAS',
+            titleColour = '#F97C81',
+
+            label = 'ID CARD',
+        
+            stamp = true,
+            profileStamp = true,
+        
+            signature = true,
+        
+            bgColour = '#f1e6db',
+            bgColourSecondary = '#ff75bc',
+        
+            textColour = '#323443',
+        },
+        
+        male_id = {
+            type = 'male_id',
+            title = 'SAN ANDREAS',
+            titleColour = '#89B1FF',
+
+            label = 'ID CARD',
+        
+            stamp = true,
+            profileStamp = true,
+        
+            signature = true,
+        
+            bgColour = '#E5FCFF',
+            bgColourSecondary = '#6FCBE9',
+        
+            textColour = '#323443',
+        },
+    }
