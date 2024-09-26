d2c22a2f16f0c76e2b6a93a7ea492d047d2be81d
DonStylz74 <84778838+DonStylz74@users.noreply.github.com>
2024-09-26 22:18:55 +1000

    Update sql-inject.sql

--- Extras/sql-inject.sql
+++ Extras/sql-inject.sql
@@ -44,21 +44,26 @@ INSERT INTO `licenses` (`type`, `label`) VALUES
 	('weed_processing', 'Weed Processing License');
 
 -- IT_Drugs SQL Injection
--- Install the drug_plants table
-CREATE TABLE IF NOT EXISTS drug_plants (id INT(11) NOT NULL AUTO_INCREMENT, PRIMARY KEY(id));
-ALTER TABLE drug_plants ADD IF NOT EXISTS coords LONGTEXT NOT NULL;
-ALTER TABLE drug_plants ADD IF NOT EXISTS time INT(255) NOT NULL;
-ALTER TABLE drug_plants ADD IF NOT EXISTS type VARCHAR(100) NOT NULL;
-ALTER TABLE drug_plants ADD IF NOT EXISTS health DOUBLE NOT NULL DEFAULT 100;
-ALTER TABLE drug_plants ADD IF NOT EXISTS fertilizer DOUBLE NOT NULL DEFAULT 0;
-ALTER TABLE drug_plants ADD IF NOT EXISTS water DOUBLE NOT NULL DEFAULT 0;
-ALTER TABLE drug_plants ADD IF NOT EXISTS growtime INT(11) NOT NULL;
+  -- Install the drug_plants table
+  CREATE TABLE IF NOT EXISTS drug_plants (
+      id VARCHAR(11) NOT NULL, PRIMARY KEY(id),
+      owner LONGTEXT DEFAULT NULL,
+      coords LONGTEXT NOT NULL,
+      time INT(255) NOT NULL,
+      type VARCHAR(100) NOT NULL,
+      health DOUBLE NOT NULL DEFAULT 100,
+      fertilizer DOUBLE NOT NULL DEFAULT 0,
+      water DOUBLE NOT NULL DEFAULT 0,
+      growtime INT(11) NOT NULL
+  );
 
--- Install the drug_processing table
-CREATE TABLE IF NOT EXISTS drug_processing (id INT(11) NOT NULL AUTO_INCREMENT, PRIMARY KEY(id));
-ALTER TABLE drug_processing ADD IF NOT EXISTS coords LONGTEXT NOT NULL;
-ALTER TABLE drug_processing ADD IF NOT EXISTS rotation DOUBLE NOT NULL;
-ALTER TABLE drug_processing ADD IF NOT EXISTS type VARCHAR(100) NOT NULL;
+  CREATE TABLE IF NOT EXISTS drug_processing (
+      id VARCHAR(11) NOT NULL, PRIMARY KEY(id),
+      coords LONGTEXT NOT NULL,
+      rotation DOUBLE NOT NULL,
+      owner LONGTEXT NOT NULL,
+      type VARCHAR(100) NOT NULL
+  );
 
 -- Install JOBGRADES pays updated table
 CREATE TABLE IF NOT EXISTS `job_grades` (
