4c1a3704e1465b58ed15b8d7f5c6f79c9a83be02
DonStylz74 <84778838+DonStylz74@users.noreply.github.com>
2024-11-05 11:40:08 +1100

    Update sql-inject.sql

--- Extras/sql-inject.sql
+++ Extras/sql-inject.sql
@@ -1,28 +1,30 @@
 INSERT INTO `vehicle_categories` (name, label) VALUES
 	('customs','Customs');
 
--- Mono mVehicle & mGarage SQL Injection
---CREATE TABLE IF NOT EXISTS `owned_vehicles` (
---  `id` int(11) NOT NULL AUTO_INCREMENT,
---  `id` int(11) NOT NULL AUTO_INCREMENT,
---  `plate` varchar(12) NOT NULL,
---  `owner` varchar(60) DEFAULT NULL,
---  `vehicle` longtext DEFAULT NULL,
--- `type` varchar(20) NOT NULL DEFAULT 'car',
---  `job` varchar(20) DEFAULT NULL,
---  `stored` tinyint(4) NOT NULL DEFAULT 0,
---  `parking` varchar(60) DEFAULT 'Pillbox Hill',
---  `pound` varchar(60) DEFAULT NULL,
---  `datetimein` INT DEFAULT 0,
---  `glovebox` longtext DEFAULT NULL,
---  `trunk` longtext DEFAULT NULL,
---  `mileage` int(11) DEFAULT 0,
---  `coords` longtext DEFAULT NULL,
---  `lastparking` varchar(100) DEFAULT NULL,
---  `keys` longtext DEFAULT '[]',
---  `metadata` longtext DEFAULT NULL,
---  PRIMARY KEY (`id`)
---) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+/*
+Mono mVehicle & mGarage SQL Injection
+CREATE TABLE IF NOT EXISTS `owned_vehicles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `plate` varchar(12) NOT NULL,
+  `owner` varchar(60) DEFAULT NULL,
+  `vehicle` longtext DEFAULT NULL,
+ `type` varchar(20) NOT NULL DEFAULT 'car',
+  `job` varchar(20) DEFAULT NULL,
+  `stored` tinyint(4) NOT NULL DEFAULT 0,
+  `parking` varchar(60) DEFAULT 'Pillbox Hill',
+  `pound` varchar(60) DEFAULT NULL,
+  `datetimein` INT DEFAULT 0,
+  `glovebox` longtext DEFAULT NULL,
+  `trunk` longtext DEFAULT NULL,
+  `mileage` int(11) DEFAULT 0,
+  `coords` longtext DEFAULT NULL,
+  `lastparking` varchar(100) DEFAULT NULL,
+  `keys` longtext DEFAULT '[]',
+  `metadata` longtext DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+*/
 
 -- esx-sna-fuel Injection
 CREATE TABLE IF NOT EXISTS `owned_vehicles` (
