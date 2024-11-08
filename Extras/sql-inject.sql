135491e962402daa157e52536eadcaf8456348e5
DonStylz74 <84778838+DonStylz74@users.noreply.github.com>
2024-11-09 10:46:51 +1100

    Update sql-inject.sql

--- Extras/sql-inject.sql
+++ Extras/sql-inject.sql
@@ -1,6 +1,3 @@
-INSERT INTO `vehicle_categories` (name, label) VALUES
-	('customs','Customs');
-
 -- JG Advance Garages
 -- player_vehicles
 ALTER TABLE `owned_vehicles` ADD COLUMN IF NOT EXISTS `fuel` INT(10) DEFAULT '100';
@@ -31,12 +28,96 @@ CREATE TABLE IF NOT EXISTS `player_priv_garages` (
     PRIMARY KEY (`id`)
 );
 
--- esx-sna-fuel Injection
-CREATE TABLE IF NOT EXISTS `owned_vehicles` (
-  `datetimein` INT DEFAULT 0,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+-- JG Dealershps
+ALTER TABLE `owned_vehicles` ADD COLUMN IF NOT EXISTS `financed` tinyint(1) NOT NULL DEFAULT 0;
+ALTER TABLE `owned_vehicles` ADD COLUMN IF NOT EXISTS `finance_data` longtext DEFAULT NULL;
 
+CREATE TABLE IF NOT EXISTS `dealership_data` (
+  `name` varchar(100) NOT NULL,
+  `label` varchar(255) NOT NULL,
+  `balance` float NOT NULL DEFAULT 0,
+  `owner_id` varchar(255) DEFAULT NULL,
+  `owner_name` varchar(255) DEFAULT NULL,
+  `employee_commission` int(11) DEFAULT '10',
+  PRIMARY KEY (`name`)
+);
+
+CREATE TABLE IF NOT EXISTS `dealership_vehicles` (
+  `spawn_code` varchar(100) NOT NULL,
+  `brand` varchar(255) DEFAULT NULL,
+  `model` varchar(255) DEFAULT NULL,
+  `hashkey` varchar(100) DEFAULT NULL,
+  `category` varchar(100) NOT NULL,
+  `price` float NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`spawn_code`)
+);
+
+CREATE TABLE IF NOT EXISTS `dealership_dispveh` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `dealership` varchar(100) NOT NULL,
+  `vehicle` varchar(100) NOT NULL,
+  `color` varchar(100) NOT NULL,
+  `coords` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_dispveh_dealership` (`dealership`),
+  KEY `fk_dispveh_vehicle` (`vehicle`)
+);
+
+CREATE TABLE IF NOT EXISTS `dealership_orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `vehicle` varchar(100) NOT NULL,
+  `dealership` varchar(100) NOT NULL,
+  `quantity` int(11) NOT NULL DEFAULT 0,
+  `cost` float NOT NULL DEFAULT 0,
+  `delivery_time` int(11) NOT NULL,
+  `order_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `fulfilled` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY `orders_vehicle_fk` (`vehicle`),
+  KEY `orders_dealership_fk` (`dealership`)
+);
+
+CREATE TABLE IF NOT EXISTS `dealership_sales` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `dealership` varchar(255) NOT NULL,
+  `vehicle` varchar(100) NOT NULL,
+  `plate` varchar(255) NOT NULL,
+  `player` varchar(255) NOT NULL,
+  `seller` varchar(255),
+  `purchase_type` varchar(50) NOT NULL,
+  `paid` float NOT NULL DEFAULT 0,
+  `owed` float NOT NULL DEFAULT 0,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  KEY `fk_sales_vehicle` (`vehicle`),
+  KEY `fk_sales_dealership` (`dealership`),
+  KEY `fk_sales_player` (`player`),
+  KEY `fk_sales_plate` (`plate`)
+);
+
+CREATE TABLE IF NOT EXISTS `dealership_stock` (
+  `dealership` varchar(100) NOT NULL,
+  `vehicle` varchar(100) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `price` float NOT NULL DEFAULT 0,
+  PRIMARY KEY (`dealership`, `vehicle`),
+  KEY `vehicle_fk` (`vehicle`)
+);
+
+CREATE TABLE IF NOT EXISTS `dealership_employees` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `identifier` varchar(255) NOT NULL,
+  `dealership` varchar(255) NOT NULL,
+  `role` varchar(100) NOT NULL,
+  `joined` datetime NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  KEY `fk_employees_dealership` (`dealership`)
+);
+
+-- Adding car categorie - CUSTOMS
+INSERT INTO `vehicle_categories` (name, label) VALUES
+	('customs','Customs');
 
 -- Licences SQL Injection
 CREATE TABLE IF NOT EXISTS `licenses` (
@@ -56,29 +137,6 @@ INSERT INTO `licenses` (`type`, `label`) VALUES
 	('waterdmv', 'Boating Permit'),
 	('weapon', 'Firearms License'),
 	('weed_processing', 'Weed Processing License');
-
--- IT_Drugs SQL Injection
-  -- Install the drug_plants table
-  CREATE TABLE IF NOT EXISTS drug_plants (
-      id VARCHAR(11) NOT NULL, PRIMARY KEY(id),
-      owner LONGTEXT DEFAULT NULL,
-      coords LONGTEXT NOT NULL,
-      time INT(255) NOT NULL,
-      type VARCHAR(100) NOT NULL,
-      health DOUBLE NOT NULL DEFAULT 100,
-      fertilizer DOUBLE NOT NULL DEFAULT 0,
-      water DOUBLE NOT NULL DEFAULT 0,
-      growtime INT(11) NOT NULL
-  );
- 
-  -- Install the drug_processing table
-  CREATE TABLE IF NOT EXISTS drug_processing (
-      id VARCHAR(11) NOT NULL, PRIMARY KEY(id),
-      coords LONGTEXT NOT NULL,
-      rotation DOUBLE NOT NULL,
-      owner LONGTEXT NOT NULL,
-      type VARCHAR(100) NOT NULL
-  );
 
 -- Install JOBGRADES pays updated table
 CREATE TABLE IF NOT EXISTS `job_grades` (
@@ -133,3 +191,31 @@ CREATE TABLE IF NOT EXISTS `tcd_starterpack` (
   `received` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- esx-sna-fuel Injection
+CREATE TABLE IF NOT EXISTS `owned_vehicles` (
+  `datetimein` INT DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- IT_Drugs SQL Injection
+  -- Install the drug_plants table
+  CREATE TABLE IF NOT EXISTS drug_plants (
+      id VARCHAR(11) NOT NULL, PRIMARY KEY(id),
+      owner LONGTEXT DEFAULT NULL,
+      coords LONGTEXT NOT NULL,
+      time INT(255) NOT NULL,
+      type VARCHAR(100) NOT NULL,
+      health DOUBLE NOT NULL DEFAULT 100,
+      fertilizer DOUBLE NOT NULL DEFAULT 0,
+      water DOUBLE NOT NULL DEFAULT 0,
+      growtime INT(11) NOT NULL
+  );
+ 
+  -- Install the drug_processing table
+  CREATE TABLE IF NOT EXISTS drug_processing (
+      id VARCHAR(11) NOT NULL, PRIMARY KEY(id),
+      coords LONGTEXT NOT NULL,
+      rotation DOUBLE NOT NULL,
+      owner LONGTEXT NOT NULL,
+      type VARCHAR(100) NOT NULL
+  );
