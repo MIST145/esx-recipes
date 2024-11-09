48340dcc56d798d4d482edb51dabedfc244ac940
DonStylz74 <84778838+DonStylz74@users.noreply.github.com>
2024-11-09 22:55:05 +1100

    Update extra.sql

--- extra.sql
+++ extra.sql
@@ -28,18 +28,3 @@ CREATE TABLE IF NOT EXISTS `player_transactions` (
    `transactions` longtext DEFAULT '[]',
   PRIMARY KEY (`id`)
 );
-
--- OX Doorlock
-CREATE TABLE IF NOT EXISTS `ox_doorlock` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `data` longtext NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
-INSERT INTO `ox_doorlock` (`id`, `name`, `data`) VALUES
-	(1, 'PDM RH Main 1', '{"coords":{"x":-1269.185546875,"y":-369.2408447265625,"z":37.1052017211914},"lockSound":"door_bolt","doors":[{"model":73386408,"coords":{"x":-1269.7930908203126,"y":-368.1114807128906,"z":37.11116027832031},"heading":298},{"model":-1152174184,"coords":{"x":-1268.5780029296876,"y":-370.37017822265627,"z":37.0992431640625},"heading":298}],"maxDistance":1,"groups":{"cardealer":1},"unlockSound":"door_bolt","lockpick":true,"state":1,"lockpickDifficulty":["easy","easy","easy","easy","easy","medium"]}'),
-	(2, 'PDM RH Main 2', '{"coords":{"x":-1260.7333984375,"y":-349.1142272949219,"z":37.11116027832031},"lockSound":"door_bolt","doors":[{"model":-1152174184,"coords":{"x":-1261.8756103515626,"y":-349.6961975097656,"z":37.11116027832031},"heading":207},{"model":73386408,"coords":{"x":-1259.59130859375,"y":-348.5322570800781,"z":37.11116027832031},"heading":207}],"maxDistance":1,"groups":{"cardealer":1},"unlockSound":"door_bolt","lockpick":true,"state":1,"lockpickDifficulty":["easy","easy","easy","easy","easy","medium"]}'),
-	(3, 'PDM RH Garage', '{"coords":{"x":-1237.8436279296876,"y":-338.19476318359377,"z":37.60726928710937},"heading":207,"holdOpen":true,"model":1430328167,"lockSound":"metal_locker","doors":false,"maxDistance":5,"autolock":10,"groups":{"cardealer":1},"unlockSound":"door_bolt","lockpick":true,"state":1,"lockpickDifficulty":["easy","easy","easy","easy","easy","medium"],"doorRate":0.2}'),
-	(4, 'PDM RH Office', '{"coords":{"x":-1243.6416015625,"y":-347.42779541015627,"z":37.47900009155273},"heading":298,"holdOpen":true,"model":-952356348,"lockSound":"metal_locker","doors":false,"maxDistance":1,"groups":{"cardealer":1},"unlockSound":"metallic_creak","lockpick":true,"state":1,"lockpickDifficulty":["easy","easy","medium"]}');
-
